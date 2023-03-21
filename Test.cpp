a3f3a31ceed136936f450993393924d3b9e1b872
aradbm <aradbm@gmail.com>
2023-03-21 19:59:08 +0200

    added 3 tests for game

--- Test.cpp
+++ Test.cpp
@@ -1,16 +1,40 @@
 #include "doctest.h"
+#include <stdexcept>
 #include "sources/player.hpp"
 #include "sources/game.hpp"
 #include "sources/card.hpp"
 using namespace ariel;
-TEST_CASE("test1: ")
+// ------------------- Player tests: -------------------
+TEST_CASE("test player constructor")
+{
+    Player p1("moshe");
+    Player p2("yosi");
+    bool v1 = p1.stacksize() == 26;
+    bool v2 = p2.stacksize() == 26;
+    CHECK(v1);
+    CHECK(v2);
+}
+// ------------------- Game tests: -------------------
+TEST_CASE("check overall stacksize mid game")
 {
     Player p1("moshe");
     Player p2("yosi");
     Game game(p1, p2);
+    bool v1 = p1.stacksize() + p2.stacksize() == 52;
     for (int i = 0; i < 5; i++)
     {
         game.playTurn();
+        CHECK(v1);
     }
-    CHECK(p1.stacksize() == p2.stacksize());
+}
+TEST_CASE("check playAll func")
+{
+    Player p1("moshe");
+    Player p2("yosi");
+    Game game(p1, p2);
+    game.playAll();
+    bool v1 = p1.stacksize() == 0 || p2.stacksize() == 0;
+    bool v2 = p1.stacksize() + p2.stacksize() == 52;
+    CHECK(v1);
+    CHECK(v2);
 }
