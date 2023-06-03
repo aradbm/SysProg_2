8899525454dd3b7e83e9ce87dfec24c4fe6d379c
aradbm <aradbm@gmail.com>
2023-06-03 21:02:55 +0300

    added sidecross functionallity

--- sources/MagicalContainer.cpp
+++ sources/MagicalContainer.cpp
@@ -2,69 +2,179 @@
 
 namespace ariel
 {
+    // MagicalContainer
+
     void MagicalContainer::addElement(int element)
     {
+        elements.push_back(element);
     }
 
     void MagicalContainer::removeElement(int element)
     {
+        auto position = std::find(elements.begin(), elements.end(), element);
+        if (position != elements.end())
+        {
+            elements.erase(position);
+        }
+        else
+        {
+            throw std::runtime_error("Element not found in the container.");
+        }
     }
 
     int MagicalContainer::size() const
     {
-        return 0;
+        return elements.size();
     }
+
+    // AscendingIterator
 
     MagicalContainer::AscendingIterator::AscendingIterator(MagicalContainer &container) : container(container)
     {
+        if (!container.elements.empty())
+        {
+            std::sort(container.elements.begin(), container.elements.end());
+            current = container.elements.data();
+        }
+        else
+        {
+            current = nullptr;
+        }
     }
 
     int *MagicalContainer::AscendingIterator::begin()
     {
-        int *temp = 0;
-        return temp;
+        return container.elements.empty() ? nullptr : &container.elements.front();
     }
 
     int *MagicalContainer::AscendingIterator::end()
     {
-        int *temp = 0;
-        return temp;
+        return container.elements.empty() ? nullptr : &container.elements.back() + 1;
     }
+
+    Iterator &MagicalContainer::AscendingIterator::operator++()
+    {
+        if (current == nullptr)
+            throw std::runtime_error("Iterator is not initialized.");
+        if (current == end())
+            throw std::runtime_error("Iterator is at the end of the container.");
+        if (current != nullptr)
+        {
+            ++current;
+            if (current == end())
+            {
+                current = end();
+            }
+        }
+        return *this;
+    }
+    // SideCrossIterator
 
     MagicalContainer::SideCrossIterator::SideCrossIterator(MagicalContainer &container) : container(container)
     {
+        if (!container.elements.empty())
+        {
+            current = container.elements.data();
+        }
+        else
+        {
+            current = nullptr;
+        }
     }
 
     int *MagicalContainer::SideCrossIterator::begin()
     {
-        int *temp = 0;
-        return temp;
+        return container.elements.empty() ? nullptr : container.elements.data();
     }
 
     int *MagicalContainer::SideCrossIterator::end()
     {
-        int *temp = 0;
-        return temp;
+        return container.elements.empty() ? nullptr : container.elements.data() + container.size();
     }
+
+    Iterator &MagicalContainer::SideCrossIterator::operator++()
+    {
+        if (current == nullptr)
+            throw std::runtime_error("Iterator is not initialized.");
+
+        if (current == end())
+            throw std::runtime_error("Iterator is at the end of the container.");
+
+        if (current != nullptr)
+        {
+            if (current == container.elements.data())
+            {
+                current++;
+            }
+            else if (current == container.elements.data() + container.size() - 1)
+            {
+                current--;
+            }
+            else
+            {
+                int distanceFromBegin = current - container.elements.data();
+                int distanceFromEnd = (container.elements.data() + container.size() - 1) - current;
+
+                if (distanceFromBegin <= distanceFromEnd)
+                {
+                    current = container.elements.data() + container.size() - 1 - distanceFromBegin;
+                }
+                else
+                {
+                    current = container.elements.data() + distanceFromEnd + 1;
+                }
+            }
+        }
+        return *this;
+    }
+
+    // PrimeIterator
 
     MagicalContainer::PrimeIterator::PrimeIterator(MagicalContainer &container) : container(container)
     {
+        current = begin();
     }
 
     bool MagicalContainer::PrimeIterator::isPrime(int number)
     {
-        return false;
+        if (number <= 1)
+            return false;
+        for (int i = 2; i * i <= number; i++)
+        {
+            if (number % i == 0)
+                return false;
+        }
+        return true;
     }
 
     int *MagicalContainer::PrimeIterator::begin()
     {
-        int *temp = 0;
-        return temp;
+        for (int *p = container.elements.data(); p != end(); p++)
+        {
+            if (isPrime(*p))
+                return p;
+        }
+        return end();
     }
 
     int *MagicalContainer::PrimeIterator::end()
     {
-        int *temp = 0;
-        return temp;
+        return container.elements.data() + container.size();
     }
+
+    Iterator &MagicalContainer::PrimeIterator::operator++()
+    {
+        if (current == nullptr)
+            throw std::runtime_error("Iterator is not initialized.");
+        if (current == end())
+            throw std::runtime_error("Iterator is at the end of the container.");
+
+        current++;
+        while (current != end() && !isPrime(*current))
+        {
+            current++;
+        }
+        return *this;
+    }
+
 }
--- sources/MagicalContainer.hpp
+++ sources/MagicalContainer.hpp
@@ -1,14 +1,44 @@
 #pragma once
 #include <vector>
+#include <stdexcept>
+#include <algorithm>
+#include <set>
 
 namespace ariel
 {
     class Iterator
     {
+    protected:
+        int *current;
+
     public:
+        // constructors
+        Iterator() : current(nullptr) {}
+        virtual ~Iterator() = default;
         virtual int *begin() = 0;
         virtual int *end() = 0;
-        virtual ~Iterator() = default;
+
+        // operators
+        virtual Iterator &operator++() = 0;
+        virtual bool operator==(int *other) const
+        {
+            if (current == nullptr && other == nullptr)
+                return true;
+            else if (current == nullptr || other == nullptr)
+                return false;
+            return *current == *other;
+        }
+        virtual bool operator==(const Iterator &other) const
+        {
+            if (current == nullptr || other.current == nullptr)
+                return false;
+            return *current == *other.current;
+        }
+        virtual bool operator!=(int *other) const { return !(*this == other); }
+        virtual bool operator!=(const Iterator &other) const { return !(*this == other); }
+        virtual bool operator<(const Iterator &other) const { return current < other.current; }
+        virtual bool operator>(const Iterator &other) const { return current > other.current; }
+        virtual int &operator*() { return *current; }
     };
 
     class MagicalContainer
@@ -36,17 +66,33 @@ namespace ariel
         AscendingIterator(MagicalContainer &container);
         int *begin() override;
         int *end() override;
+        Iterator &operator++() override;
+        AscendingIterator &operator=(const AscendingIterator &other)
+        {
+            if (this != &other)
+                current = other.current;
+            return *this;
+        }
     };
 
     class MagicalContainer::SideCrossIterator : public Iterator
     {
     private:
         MagicalContainer &container;
+        int currentIndex;
+        bool isReversed;
 
     public:
         SideCrossIterator(MagicalContainer &container);
         int *begin() override;
         int *end() override;
+        Iterator &operator++() override;
+        SideCrossIterator &operator=(const SideCrossIterator &other)
+        {
+            if (this != &other)
+                current = other.current;
+            return *this;
+        }
     };
 
     class MagicalContainer::PrimeIterator : public Iterator
@@ -59,5 +105,13 @@ namespace ariel
         bool isPrime(int number);
         int *begin() override;
         int *end() override;
+        Iterator &operator++() override;
+
+        PrimeIterator &operator=(const PrimeIterator &other)
+        {
+            if (this != &other)
+                current = other.current;
+            return *this;
+        }
     };
 }
