bac31213df57a3f6661632cca8c416e8fce912de
aradbm <aradbm@gmail.com>
2023-06-11 20:56:54 +0300

    restored 5a test

--- Test.cpp
+++ Test.cpp
@@ -1,126 +1,149 @@
-#include "MagicalContainer.hpp"
-#include <iostream>
 #include "doctest.h"
-using namespace std;
+#include "sources/MagicalContainer.hpp"
+#include <stdexcept>
+#include <iostream>
+
 using namespace ariel;
 
-TEST_CASE("Sum of Elements")
+TEST_CASE("Adding elements")
 {
     MagicalContainer container;
-    container.addElement(2);
-    container.addElement(4);
-    container.addElement(6);
-
-    CHECK(container.size() == 3);
-
-    MagicalContainer::AscendingIterator ascendingIter(container);
-    int sum = 0;
-    for (int *ptr = ascendingIter.begin(); ptr != ascendingIter.end(); ++ptr)
-    {
-        sum += *ptr;
-    }
-
-    CHECK(sum == 12);
-    CHECK(sum != 10);
-    CHECK(sum > 0);
-}
-
-TEST_CASE("Count Prime Numbers")
-{
-    MagicalContainer container;
-
-    container.addElement(3);
-    container.addElement(4);
-    container.addElement(7);
-    container.addElement(9);
-
+    container.addElement(42);
+    CHECK(container.size() == 1);
+    container.addElement(10);
+    container.addElement(20);
+    container.addElement(30);
     CHECK(container.size() == 4);
-
-    MagicalContainer::PrimeIterator primeIter(container);
-    int count = 0;
-    for (int *ptr = primeIter.begin(); ptr != primeIter.end(); ++ptr)
-    {
-        ++count;
-    }
-
-    CHECK(count == 2);
-    CHECK(count != 3);
-    CHECK(count > 0);
-    CHECK(count <= 2);
 }
 
-TEST_CASE("Add and Remove Elements")
+TEST_CASE("Removing elements")
 {
     MagicalContainer container;
-
-    container.addElement(2);
-    container.addElement(4);
-    container.addElement(6);
-    container.removeElement(4);
-    container.addElement(8);
-
+    container.addElement(42);
+    container.addElement(10);
+    container.addElement(20);
+    container.addElement(30);
+    container.removeElement(10);
+    CHECK(container.size() == 3);
+    CHECK_THROWS_AS(container.removeElement(50), std::runtime_error);
     CHECK(container.size() == 3);
 }
 
-TEST_CASE("Ascending Iterator with Negative Numbers")
+TEST_CASE("Ascending iterator")
+{
+    MagicalContainer container;
+    container.addElement(42);
+    container.addElement(10);
+    container.addElement(20);
+    container.addElement(30);
+
+    int expected[] = {10, 20, 30, 42};
+    int index = 0;
+    MagicalContainer::AscendingIterator it(container);
+    while (*it != *(it.end()))
+    {
+        CHECK(*it == expected[index]);
+        ++index;
+        ++it;
+    }
+}
+
+TEST_CASE("Side cross iterator")
+{
+    MagicalContainer container;
+    container.addElement(42);
+    container.addElement(10);
+    container.addElement(20);
+    container.addElement(30);
+    int expected[] = {42, 30, 10, 20};
+    int index = 0;
+
+    MagicalContainer::SideCrossIterator it(container);
+    while (*it != *(it.end()))
+    {
+        CHECK(*it == expected[index]);
+        ++index;
+        ++it;
+    }
+}
+
+TEST_CASE("Prime iterator")
+{
+    // Creating an instance of MagicalContainer
+    MagicalContainer container;
+
+    // Adding four elements to the container
+    container.addElement(42);
+    container.addElement(10);
+    container.addElement(20);
+    container.addElement(7);
+
+    // Creating an array of expected values
+    int expected[] = {7};
+    int index = 0;
+
+    MagicalContainer::PrimeIterator it(container);
+
+    // Iterating over the container using a prime iterator
+    while (*it != *(it.end()))
+    {
+        // Checking if the current value matches the expected value
+        CHECK(*it == expected[index]);
+        ++index;
+        ++it;
+    }
+}
+
+TEST_CASE("Iterator comparison")
+{
+    MagicalContainer container;
+    container.addElement(42);
+    container.addElement(10);
+    container.addElement(20);
+    container.addElement(30);
+    MagicalContainer::AscendingIterator ascIt1(container);
+    MagicalContainer::AscendingIterator ascIt2(container);
+
+    CHECK(ascIt1.begin() == ascIt2.begin());
+
+    MagicalContainer::SideCrossIterator sideIt1(container);
+    MagicalContainer::SideCrossIterator sideIt2(container);
+    CHECK(sideIt1.begin() == sideIt2.begin());
+
+    MagicalContainer::PrimeIterator primeIt1(container);
+    MagicalContainer::PrimeIterator primeIt2(container);
+    CHECK(primeIt1 == primeIt2);
+    ++ascIt2;
+    CHECK(ascIt1 != ascIt2);
+    CHECK(ascIt1 < ascIt2);
+    CHECK(ascIt2 > ascIt1);
+    ++sideIt2;
+    CHECK(sideIt1 != sideIt2);
+    CHECK(sideIt1 < sideIt2);
+    CHECK(sideIt2 > sideIt1);
+    ++primeIt2;
+    CHECK(primeIt1 != primeIt2);
+    CHECK(primeIt1 < primeIt2);
+    CHECK(primeIt2 > primeIt1);
+}
+
+TEST_CASE("Ascending iterator (negative, positive)")
 {
     MagicalContainer container;
 
-    container.addElement(-5);
     container.addElement(-10);
-    container.addElement(-15);
+    container.addElement(20);
+    container.addElement(-30);
+    container.addElement(40);
+    int expected[] = {-30, -10, 20, 40};
+    int index = 0;
 
-    MagicalContainer::AscendingIterator ascendingIter(container);
+    MagicalContainer::AscendingIterator it(container);
 
-    int sum = 0;
-    for (int *ptr = ascendingIter.begin(); ptr != ascendingIter.end(); ++ptr)
+    while (*it != *(it.end()))
     {
-        sum += *ptr;
+        CHECK(*it == expected[index]);
+        ++index;
+        ++it;
     }
-
-    CHECK(sum == -30);
-    CHECK(sum != 0);
-    CHECK(sum < 0);
-}
-
-TEST_CASE("Remove Nonexistent Element")
-{
-    MagicalContainer container;
-
-    container.addElement(2);
-    container.addElement(4);
-    container.addElement(6);
-
-    container.removeElement(8);
-
-    CHECK(container.size() == 3);
-}
-
-TEST_CASE("Ascending Iterator")
-{
-    MagicalContainer container;
-
-    container.addElement(-5);
-    container.addElement(-10);
-    container.addElement(-15);
-
-    CHECK(container.size() == 3);
-    CHECK(container.size() != 0);
-
-    MagicalContainer::AscendingIterator ascendingIter(container);
-
-    int sum = 0;
-    for (int *ptr = ascendingIter.begin(); ptr != ascendingIter.end(); ++ptr)
-    {
-        sum += *ptr;
-    }
-
-    CHECK(sum == -30);
-    CHECK(sum != 0);
-    CHECK(sum < 0);
-    CHECK(ascendingIter.begin() != nullptr);
-    CHECK(ascendingIter.end() != nullptr);
-    CHECK(container.size() >= 2);
-    CHECK(container.size() < 5);
-    CHECK(container.size() > 0);
 }
